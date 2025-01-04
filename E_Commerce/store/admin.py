433bc454ae152284076123b7dfc5c2b7a3184bdf
Haris Khan <hariskhan3428@gmail.com>
2025-01-04 22:41:10 +0500

    created order model

--- E_Commerce/store/admin.py
+++ E_Commerce/store/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
-from .models import Category, Sub_Category, Product
+from .models import Category, Sub_Category, Product, Contact, Order
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')  # Add the fields you want to display
 
 class SubCategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category')
+    list_display = ('id','name', 'category')
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'date')
+    list_display = ('id','name', 'price', 'date', 'category', 'sub_category', 'image')
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'message')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user','product', 'quantity', 'price', 'address', 'phone', 'date')
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Sub_Category, SubCategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Order, OrderAdmin)
--- E_Commerce/store/models.py
+++ E_Commerce/store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -34,3 +35,13 @@ class Contact(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default='')
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    address = models.TextField()
+    phone = models.CharField(max_length=11)
+    date = models.DateField(auto_now_add=True)
+
